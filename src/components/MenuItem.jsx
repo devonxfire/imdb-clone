97ebdde36a6bec8d88d0f793e326da7dd6140e9b
devonmartindale <devonmartindale@gmail.com>
2024-04-19 14:48:29 +0200

    add header component

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function MenuItem({ title, address, Icon }) {
+  return (
+    <Link href={address}>
+      <Icon className="text-2xl sm:hidden hover:text-amber-500" />
+      <p className="uppercase hidden sm:inline text-sm">{title}</p>
+    </Link>
+  );
+}
