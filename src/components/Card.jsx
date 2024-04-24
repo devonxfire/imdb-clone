05be711e17a881ef994d3949244e4c4094162914
devonmartindale <devonmartindale@gmail.com>
2024-04-24 12:14:52 +0200

    update card link

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,28 +6,29 @@ import { FaThumbsUp } from "react-icons/fa";
 export default function MovieCard({ result }) {
   return (
     <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2  duration-200 sm:hover:scale-105 transition-transform  ">
-      <Link href={`/movie/${result.id}`}></Link>
-      <Image
-        src={`https://image.tmdb.org/t/p/original/${
-          result.backdrop_path || result.poster_path
-        }`}
-        alt="movie poster"
-        width={500}
-        height={300}
-        className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
-      />
-      <div className="p-2">
-        {" "}
-        <p className="line-clamp-2 text-md">{result.overview}</p>
-        <h2 className="text-lg font-bold truncate mt-2">
-          {result.title || result.name}
-        </h2>
-        <p className="flex items-center mt-2 text-sm">
-          {result.release_date || result.first_air_date}
-          <FaThumbsUp className="h-5 mr-1 ml-3" />
-          {result.vote_count}
-        </p>
-      </div>
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          alt="movie poster"
+          width={500}
+          height={300}
+          className="sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300"
+        />
+        <div className="p-2">
+          {" "}
+          <p className="line-clamp-2 text-md">{result.overview}</p>
+          <h2 className="text-lg font-bold truncate mt-2">
+            {result.title || result.name}
+          </h2>
+          <p className="flex items-center mt-2 text-sm">
+            {result.release_date || result.first_air_date}
+            <FaThumbsUp className="h-5 mr-1 ml-3" />
+            {result.vote_count}
+          </p>
+        </div>
+      </Link>
     </div>
   );
 }
