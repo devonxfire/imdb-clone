b02c9efb586467eabeb9095b378485714391caff
devonmartindale <devonmartindale@gmail.com>
2024-04-24 12:09:05 +0200

    update layout page

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -2,25 +2,22 @@
 
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 
 export default function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
   return (
-    <Suspense>
-      <div>
-        <Link
-          className={`hover:text-amber-600 font-semibold ${
-            genre === param
-              ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-              : ""
-          }`}
-          href={`/?genre=${param}`}
-        >
-          {title}
-        </Link>
-      </div>
-    </Suspense>
+    <div>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param
+            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            : ""
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
   );
 }
